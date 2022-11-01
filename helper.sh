01f63a311237a487902cd51e251719a559e0d664
Mackenzie James <mackenzie.14@hotmail.co.uk>
2022-11-01 12:07:03 +0000

    Update helper.sh
    added help and update-system commands

--- helper.sh
+++ helper.sh
@@ -15,3 +15,22 @@ if [ $# -eq 0 ]; then
     echo "  help - print this help message"
     exit 0
 fi
+
+# create help that gives a list of commands
+if [ $1 = "help" ]; then
+    echo -e "${BLUE}Usage: $1 ${ENDCOLOR}"
+    echo -e "${BLUE}Commands ${ENDCOLOR}"
+    echo -e "${BLUE}  update-system - update all apt installed resouces and packages${ENDCOLOR}"
+    echo -e "${BLUE}  install-wp - installs wordpress${ENDCOLOR}"
+    echo -e "${BLUE}  directory-check - installs wordpress${ENDCOLOR}"
+    exit 0
+fi
+
+# if the first argument is "update-system", run the update system command
+if [ $1 = "update-system" ]; then
+    echo -e "${BLUE}Updating System...${ENDCOLOR}"
+    sudo apt-get update && sudo apt-get upgrade -y
+    wait
+    echo -e "${GREEN}System Updated!${ENDCOLOR}"
+    exit 0
+fi
